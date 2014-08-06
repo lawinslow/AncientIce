92971815d6f438dd3b742e5a92e5c1a9a3977fdb
rbatt <battrd@gmail.com>
2014-08-06 14:12:52 -0400

    tobit regression coefficients

--- R/aiTobit.R
+++ R/aiTobit.R
@@ -1,7 +1,7 @@
 
 
 library(VGAM)
-
+library(zoo)
 
 
 tornio.preds <- c("co2", "nao.djfm", "aod", "sunspots") # add air.t.mam
@@ -142,44 +142,31 @@ suwa.after.i <- suwa[,"year"] >= 1897 & suwa[,"year"] <= 1997
 suwa.1 <- suwa[suwa.before.i,]
 suwa.2 <- suwa[suwa.after.i,]
 
-suwa.preds <- c("co2", "enso", "aod", "air.t.as", "reff")
-suwa.pred.combn <- list()
+suwa.preds <- c("year", "co2", "enso", "air.t.as", "aod")
+
 for(i in 1:length(suwa.preds)){
-	suwa.pred.combn <- c(suwa.pred.combn, combn(suwa.preds, i, simplify=FALSE))
+	suwa.1[,suwa.preds[i]] <- scale(suwa.1[,suwa.preds[i]])
+	suwa.2[,suwa.preds[i]] <- scale(suwa.2[,suwa.preds[i]])
 }
 
-# Suwa Tobit before breakpoint
-ts1.minsofar <- 1E12 # starting nll for accumulating minima
-ts2.minsofar <- 1E12 # same, but for later half
-ts1.nlls <- c()
-ts2.nlls <- c()
-for(i in 1:length(suwa.pred.combn)){
+
+iceTobit.s1 <- data.frame("water"=NA, "period"=NA, "variable"=NA, "estimate"=NA, "stdE"=NA, "Z"=NA)
+iceTobit.s2 <- data.frame("water"=NA, "period"=NA, "variable"=NA, "estimate"=NA, "stdE"=NA, "Z"=NA)
+for(i in 1:length(suwa.preds)){
+	t.ts.formula <- as.formula(paste("doy~", paste(suwa.preds[i], collapse="+"), sep=""))
 	
-	t.ts1.formula <- as.formula(paste("doy~", paste(suwa.pred.combn[[i]], collapse="+"), sep=""))
-	t.ts1 <- vglm(t.ts1.formula, tobit(Lower=min.suwa, Upper=max.suwa), data=suwa.1)
-	t.ts1.nll <- t.ts1@criterion$loglikelihood
-	ts1.nlls[i] <- t.ts1.nll
-	t.ts1.min <- t.ts1.nll
-	if(t.ts1.min < ts1.minsofar){
-		ts1.minsofar <- t.ts1.min # the nll value of the best model, so far
-		ts1.min <- i # the index of the smallest nll for the model, so far
-		ts1 <- t.ts1
-	}
+	# do the before period
+	t.ts1 <- vglm(t.ts.formula, tobit(Lower=min.suwa, Upper=max.suwa), data=suwa.1)
+	t.ts1.s <- summary(t.ts1)@coef3
+	iceTobit.s1[i,] <- c("suwa", "before", rownames(t.ts1.s)[3], t.ts1.s[3,"Estimate"], t.ts1.s[3,"Std. Error"], t.ts1.s[3,"z value"])
 	
-	
-	t.ts2.formula <- as.formula(paste("doy~", paste(suwa.pred.combn[[i]], collapse="+"), sep=""))
-	t.ts2 <- vglm(t.ts2.formula, tobit(Lower=min.suwa, Upper=max.suwa), data=suwa.2)
-	t.ts2.nll <- t.ts2@criterion$loglikelihood
-	t.ts2.min <- t.ts2.nll
-	ts2.nlls[i] <- t.ts2.nll
-	if(t.ts2.min < ts2.minsofar){
-		ts2.minsofar <- t.ts2.min # the nll value of the best model, so far
-		ts2.min <- i # the index of the smallest nll for the model, so far
-		ts2 <- t.ts2
-	}
+	# do the after period
+	t.ts2 <- vglm(t.ts.formula, tobit(Lower=min.suwa, Upper=max.suwa), data=suwa.2)
+	t.ts2.s <- summary(t.ts2)@coef3
+	iceTobit.s2[i,] <- c("suwa", "after", rownames(t.ts2.s)[3], t.ts2.s[3,"Estimate"], t.ts2.s[3,"Std. Error"], t.ts2.s[3,"z value"])
 	
 }
-
+iceTobit.s <- rbind(iceTobit.s1, iceTobit.s2)
 
 
 # ===============================================
@@ -193,47 +180,111 @@ tornio.after.i <- tornio[,"year"] >= 1921 & tornio[,"year"] <= 2000
 tornio.1 <- tornio[tornio.before.i,]
 tornio.2 <- tornio[tornio.after.i,]
 
-tornio.preds <- c("air.t.stock", "sunspots", "aod", "nao.djfm", "co2")
-tornio.pred.combn <- list()
+tornio.preds <- c("year", "co2", "nao.djfm", "air.t.stock", "aod", "sunspots")
+
 for(i in 1:length(tornio.preds)){
-	tornio.pred.combn <- c(tornio.pred.combn, combn(tornio.preds, i, simplify=FALSE))
-}
-
-# Tornio Tobit before breakpoint
-tt1.minsofar <- 1E12 # starting nll for accumulating minima
-tt2.minsofar <- 1E12 # same, but for later half
-tt1.nlls <- c()
-tt2.nlls <- c()
-for(i in 1:length(tornio.pred.combn)){
-	
-	t.tt1.formula <- as.formula(paste("doy~", paste(tornio.pred.combn[[i]], collapse="+"), sep=""))
-	t.tt1 <- vglm(t.tt1.formula, tobit(Lower=min.tornio, Upper=max.tornio), data=tornio.1)
-	t.tt1.nll <- t.tt1@criterion$loglikelihood
-	tt1.nlls[i] <- t.tt1.nll
-	t.tt1.min <- t.tt1.nll
-	if(t.tt1.min < tt1.minsofar){
-		tt1.minsofar <- t.tt1.min # the nll value of the best model, so far
-		tt1.min <- i # the index of the smallest nll for the model, so far
-		tt1 <- t.tt1 # tornio tobit, part 1
-	}
-	
-	
-	t.tt2.formula <- as.formula(paste("doy~", paste(tornio.pred.combn[[i]], collapse="+"), sep=""))
-	t.tt2 <- vglm(t.tt2.formula, tobit(Lower=min.tornio, Upper=max.tornio), data=tornio.2)
-	t.tt2.nll <- t.tt2@criterion$loglikelihood
-	t.tt2.min <- t.tt2.nll
-	tt2.nlls[i] <- t.tt2.nll
-	if(t.tt2.min < tt2.minsofar){
-		tt2.minsofar <- t.tt2.min # the nll value of the best model, so far
-		tt2.min <- i # the index of the smallest nll for the model, so far
-		tt2 <- t.tt2 # tornio tobit, part 2
-	}
-	
+	tornio.1[,tornio.preds[i]] <- scale(tornio.1[,tornio.preds[i]])
+	tornio.2[,tornio.preds[i]] <- scale(tornio.2[,tornio.preds[i]])
 }
 
 
+iceTobit.t1 <- data.frame("water"=NA, "period"=NA, "variable"=NA, "estimate"=NA, "stdE"=NA, "Z"=NA)
+iceTobit.t2 <- data.frame("water"=NA, "period"=NA, "variable"=NA, "estimate"=NA, "stdE"=NA, "Z"=NA)
+for(i in 1:length(tornio.preds)){
+	t.tt.formula <- as.formula(paste("doy~", paste(tornio.preds[i], collapse="+"), sep=""))
+	
+	# do the before period
+	t.tt1 <- vglm(t.tt.formula, tobit(Lower=min.tornio, Upper=max.tornio), data=tornio.1)
+	t.tt1.s <- summary(t.tt1)@coef3
+	iceTobit.t1[i,] <- c("tornio", "before", rownames(t.tt1.s)[3], t.tt1.s[3,"Estimate"], t.tt1.s[3,"Std. Error"], t.tt1.s[3,"z value"])
+	
+	# do the after period
+	t.tt2 <- vglm(t.tt.formula, tobit(Lower=min.tornio, Upper=max.tornio), data=tornio.2)
+	t.tt2.s <- summary(t.tt2)@coef3
+	iceTobit.t2[i,] <- c("tornio", "after", rownames(t.tt2.s)[3], t.tt2.s[3,"Estimate"], t.tt2.s[3,"Std. Error"], t.tt2.s[3,"z value"])
+	
+}
+iceTobit.t <- rbind(iceTobit.t1, iceTobit.t2)
 
 
+iceTobit <- rbind(iceTobit.s, iceTobit.t)
+iceTobit[,"estimate"] <- as.numeric(iceTobit[,"estimate"])
+iceTobit[,"stdE"] <- as.numeric(iceTobit[,"stdE"])
+iceTobit[,"Z"] <- as.numeric(iceTobit[,"Z"])
+
+iceTobit <- iceTobit[order(iceTobit[,"water"], iceTobit[,"variable"]),]
+
+# ===========================
+# = plot tobit coefficients =
+# ===========================
+dev.new(width=3.5, height=5)
+par(mfrow=c(2,1), mar=c(2.5, 3, 0.5, 0.5), mgp=c(1.5, 0.5, 0), tcl=-0.15, ps=9, cex=1, family="Times")
+
+
+# =========================
+# = Plot just Suwa coeffs =
+# =========================
+iceTobit.s[,"estimate"] <- as.numeric(iceTobit.s[,"estimate"])
+iceTobit.s[,"stdE"] <- as.numeric(iceTobit.s[,"stdE"])
+iceTobit.s[,"Z"] <- as.numeric(iceTobit.s[,"Z"])
+
+iceTobit.s <- iceTobit.s[order(iceTobit.s[,"water"], iceTobit.s[,"variable"]),]
+
+ity0.s <- iceTobit.s[,"estimate"]-iceTobit.s[,"stdE"] # y0's for arrows()
+ity1.s <- iceTobit.s[,"estimate"]+iceTobit.s[,"stdE"] # y1's for arrows()
+it.ylim.l.s <- min(ity0.s)
+it.ylim.u.s <- max(ity1.s)
+it.ylim.s <- c(it.ylim.l.s, it.ylim.u.s)
+
+adj.x0.s <- (1:nrow(iceTobit.s))%%2
+adj.x0.s[1] <- 0
+adj.x.s <- cumsum(adj.x0.s*2)
+xvals.s <- (1:nrow(iceTobit.s))+adj.x.s
+labloc.s <- rollapply(xvals.s, width=2, by=2, mean)
+
+plot(xvals.s, iceTobit.s[,"estimate"], ylim=it.ylim.s, col=c("blue","red"), pch=19, xaxt="n", xlab="", ylab="Suwa\nCoefficient")
+text(labloc.s, y=it.ylim.l.s*1.4, unique(iceTobit.s[,"variable"]), xpd=TRUE)
+axis(side=1, at=labloc.s, labels=FALSE)
+abline(h=0, lty="dotted")
+legend("topleft", legend=c("1581 – 1681", "1897 – 1997"), text.col=c("blue","red"), bty="n", inset=c(-0.1,-0.065))
+arrows(x0=xvals.s, y0=rep(iceTobit.s[,"estimate"],2), x1=xvals.s, y1=c(ity0.s,ity1.s), length=0.05, col=c("blue","red"), angle=90)
+
+# =================================
+# = Plot just Tornio coefficients =
+# =================================
+iceTobit.t[,"estimate"] <- as.numeric(iceTobit.t[,"estimate"])
+iceTobit.t[,"stdE"] <- as.numeric(iceTobit.t[,"stdE"])
+iceTobit.t[,"Z"] <- as.numeric(iceTobit.t[,"Z"])
+
+iceTobit.t <- iceTobit.t[order(iceTobit.t[,"water"], iceTobit.t[,"variable"]),]
+
+ity0.t <- iceTobit.t[,"estimate"]-iceTobit.t[,"stdE"] # y0's for arrows()
+ity1.t <- iceTobit.t[,"estimate"]+iceTobit.t[,"stdE"] # y1's for arrows()
+it.ylim.l.t <- min(ity0.t)
+it.ylim.u.t <- max(ity1.t)
+it.ylim.t <- c(it.ylim.l.t, it.ylim.u.t)
+
+adj.x0.t <- (1:nrow(iceTobit.t))%%2
+adj.x0.t[1] <- 0
+adj.x.t <- cumsum(adj.x0.t*2)
+xvals.t <- (1:nrow(iceTobit.t))+adj.x.t
+labloc.t <- rollapply(xvals.t, width=2, by=2, mean)
+
+plot(xvals.t, iceTobit.t[,"estimate"], ylim=it.ylim.t, col=c("blue","red"), pch=19, xaxt="n", xlab="", ylab="Tornio\nCoefficient")
+text(labloc.t, y=it.ylim.l.t*1.2, unique(iceTobit.t[,"variable"]), xpd=TRUE)
+axis(side=1, at=labloc.t, labels=FALSE)
+abline(h=0, lty="dotted")
+legend("topright", legend=c("1803 – 1882", "1921 – 2000"), text.col=c("blue", "red"), bty="n", inset=c(-0.02,-0.07))
+arrows(x0=xvals.t, y0=rep(iceTobit.t[,"estimate"],2), x1=xvals.t, y1=c(ity0.t,ity1.t), length=0.05, col=c("blue","red"), angle=90)
+
+
+# ============================================
+# = Write results of tobit analysis to table =
+# ============================================
+iceTobit[,"estimate"] <- as.numeric(iceTobit[,"estimate"])
+iceTobit[,"stdE"] <- as.numeric(iceTobit[,"stdE"])
+iceTobit[,"Z"] <- as.numeric(iceTobit[,"Z"])
+write.table(iceTobit, file="/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Results/tobit_coefficients.csv", sep=",", col.names=TRUE, row.names=FALSE)
 
 # ===============================================================
 # = Plot time series with breakpoint and residual variance/ R^2 =
