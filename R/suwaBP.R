867ebc558c9d36f17d22461174c619552f829c4b
Ryan Batt <battrd@gmail.com>
2014-11-13 16:49:45 -0500

    suwa and tornio BP's separate; save Results

--- R/suwaBP.R
+++ R/suwaBP.R
@@ -1,7 +1,13 @@
 
+# ==================
+# = Load Libraries =
+# ==================
+library(VGAM)
 
 
-
+# =============
+# = Load Data =
+# =============
 suwa <- read.table("/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Data/suwa.tsv", sep="\t", header=TRUE)
 max.suwa <- max(suwa[,"doy"], na.rm=TRUE)
 min.suwa <- min(suwa[,"doy"], na.rm=TRUE)
@@ -16,11 +22,11 @@ suwa[,"year2"] <- 1:nrow(suwa)
 # =========================================
 # = Suwa: Calculate Breakpoint with Tobit =
 # =========================================
-bp.opts <- 10:(nrow(suwa)-10)
-resid.vars <- rep(NA, length(bp.opts))
-s.bp.pb <- txtProgressBar(min=1, max=length(bp.opts), style=3)
-for(i in 1:length(bp.opts)){
-	t.bp <- bp.opts[i]
+bp.opts.s <- 10:(nrow(suwa)-10)
+resid.vars <- rep(NA, length(bp.opts.s))
+s.bp.pb <- txtProgressBar(min=1, max=length(bp.opts.s), style=3)
+for(i in 1:length(bp.opts.s)){
+	t.bp <- bp.opts.s[i]
 	t.bp.year <- suwa[t.bp,"year"]
 	t.suwa <- suwa
 	t.suwa[,"bp"] <- (1:nrow(t.suwa))>=t.bp
@@ -39,9 +45,18 @@ for(i in 1:length(bp.opts)){
 	}
 	setTxtProgressBar(s.bp.pb, i)
 }
-suwa.bp <- suwa[bp.opts[which.min(resid.vars)],"year"]
-suwa.bp
-suwa.bp.i <- suwa[,"year"] < suwa.bp
 
-save(suwa.bp, suwa.bp.i, resid.vars, suwa, file="/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Resuts/suwaBP.RData")
+
+# =================================
+# = Define Breakpoint and Indices =
+# =================================
+suwa.bp <- suwa[bp.opts.s[which.min(resid.vars)],"year"]
+suwa.bp
+suwa.bp.i <- suwa[,"year"] < suwa.bp # indices in units of year2
+
+
+# ================
+# = Save Results =
+# ================
+save(suwa.bp, suwa.bp.i, resid.vars, suwa, max.suwa, min.suwa, bp.opts.s, file="/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Results/suwaBP.RData")
 
---
+++ R/tornioBP.R
@@ -0,0 +1,48 @@
+
+
+# =============
+# = Load Data =
+# =============
+tornio <- read.table("/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Data/tornio.tsv", sep="\t", header=TRUE)
+max.tornio <- max(tornio[,"doy"], na.rm=TRUE)
+min.tornio <- min(tornio[,"doy"], na.rm=TRUE)
+tornio[,"year2"] <- 1:nrow(tornio)
+
+
+# ==========
+# = Tornio =
+# ==========
+# =========================================
+# = Tornio: Calculate breakpoint with OLS =
+# =========================================
+bp.opts.t <- 1:nrow(tornio)
+r2.tornio <- rep(NA, length(bp.opts.t))
+max.sofar <- NA
+for(i in 1:length(bp.opts)){
+	t.bp <- bp.opts.t[i]
+	t.tornio <- tornio
+	t.tornio[,"bp"] <- (1:nrow(t.tornio))>=t.bp
+	# tobit.tornio.year <- lm(doy ~ year2:bp, data=t.tornio) # 1886
+	# tobit.tornio.year <- lm(doy ~ year2*bp, data=t.tornio) # 1807
+	tobit.tornio.year <- lm(doy ~ year + pmax(I(year-bp),0), data=t.tornio) # 1807
+	r2.tornio[i] <- summary(tobit.tornio.year)$r.squared
+	
+	max.sofar <- which.max(r2.tornio)
+	if(max.sofar!=i){
+		max.tobit.tornio.year <- tobit.tornio.year
+	}
+}
+
+# =================================
+# = Define Breakpoint and Indices =
+# =================================
+tornio.bp <- tornio[bp.opts.t[which.max(r2.tornio)],"year"]
+tornio.bp
+tornio.bp.i <- tornio[,"year"] < tornio.bp # indices in units of year2
+
+
+# ================
+# = Save Results =
+# ================
+save(tornio.bp, tornio.bp.i, r2.tornio, tornio, min.tornio, max.tornio, bp.opts.t, file="/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Results/tornioBP.RData")
+
