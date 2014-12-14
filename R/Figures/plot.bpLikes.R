4decbd7d821bed22dc11a3dc889b82c398419505
Ryan Batt <battrd@gmail.com>
2014-12-13 19:09:29 -0500

    check for 2 BP's in tornio

--- R/Figures/plot.bpLikes.R
+++ R/Figures/plot.bpLikes.R
@@ -39,4 +39,39 @@ mtext(bquote(AIC), side=4, line=1.5)
 abline(v=tornio.bp, lwd=2, lty="dashed")
 dev.off()
 
-# tornio[bp.opts.t, "year"][order(r2.tornio, decreasing=TRUE)]
+
+# ============================
+# = Plot Tornio 2 BP Surface =
+# ============================
+myCol <- function(n){
+	colorRampPalette(c("#000099", "#00FEFF", "#45FE4F", "#FCFF00", "#FF9400", "#FF3100"))(n)
+}
+bp2.mat <- matrix(NA, nrow=nrow(tornio), ncol=nrow(tornio), dimnames=list(unique(tornio[,"year"]), unique(tornio[,"year"])))
+bp2.mat[t(upper.tri(bp2.mat))] <- aic.tornio2
+bp2.mat <- t(bp2.mat)
+
+smplt <- c(0.8,0.85, 0.15,0.75)
+bgplt <- c(0.1,0.95,0.1,0.95)
+axargs <- list(mgp=c(1.1,0.3,0))
+# dev.new(width=5, height=5)
+png("/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Figures/tornio.2BP.surface.png", width=5, height=5, res=150, units="in")
+par(mar=c(2,2,0.1,2), ps=10, cex=1, family="Times", mgp=c(1.25,0.25,0), tcl=-0.15)
+image.plot(
+	x=as.integer(rownames(bp2.mat)), 
+	y=as.integer(colnames(bp2.mat)), 
+	z=bp2.mat-min(aic.tornio), 
+	xlab=bquote(1^st~~Breakpoint), 
+	ylab=bquote(2^nd~~Breakpoint), 
+	legend.lab=bquote(Delta~AIC~~(Relative~~to~~Best~~1~~BP~~Model)),
+	smallplot=smplt, 
+	bigplot=bgplt,
+	legend.line=1,
+	axis.args=axargs
+)
+abline(a=0, b=1)
+abline(a=50, b=1)
+abline(a=100, b=1)
+text(1850, 1850, bquote(phantom()%up%phantom()~~BP>=1~~yr~~apart~~bold(phantom()%up%phantom())), srt=45, pos=3, offset=0)
+text(1850, 1900, bquote(phantom()%up%phantom()~~BP>=50~~yrs~~apart~~bold(phantom()%up%phantom())), srt=45, pos=3, offset=0)
+text(1850, 1950, bquote(phantom()%up%phantom()~~BP>=100~~yrs~~apart~~bold(phantom()%up%phantom())), srt=45, pos=3, offset=0)
+dev.off() 
--- R/tornioBP.R
+++ R/tornioBP.R
@@ -18,24 +18,35 @@ tornio[,"year2"] <- 1:nrow(tornio)
 bp.opts.t <- 1:nrow(tornio)
 # r2.tornio <- rep(NA, length(bp.opts.t))
 aic.tornio <- rep(NA, length(bp.opts.t))
-max.sofar <- NA
 for(i in 1:length(bp.opts.t)){
 	t.bp.t <- bp.opts.t[i]
 	t.bp.year.t <- tornio[t.bp.t,"year"]
 	t.tornio <- tornio
 	t.tornio[,"bp"] <- (1:nrow(t.tornio))>=t.bp.t
-	# tobit.tornio.year <- lm(doy ~ year2:bp, data=t.tornio) # 1886
-	# tobit.tornio.year <- lm(doy ~ year2*bp, data=t.tornio) # 1807
-	tobit.tornio.year <- lm(doy ~ year + pmax(I(year-t.bp.year.t),0), data=t.tornio) # 1807
-	# r2.tornio[i] <- summary(tobit.tornio.year)$r.squared
+	tobit.tornio.year <- lm(doy ~ year + pmax(I(year-t.bp.year.t),0), data=t.tornio) # 1867
 	aic.tornio[i] <- extractAIC(tobit.tornio.year)[2]
-	
-	# best.sofar <- which.min(aic.tornio)
-	# if(best.sofar==i){
-	# 	best.tobit.tornio.year <- tobit.tornio.year
-	# }
 }
 
+
+# ==================
+# = Tornio: 2 BP's =
+# ==================
+bp.opts.t2 <- t(combn(1:nrow(tornio), 2))
+# bp.opts.t2 <- bp.opts.t2[bp.opts.t2[,2]-bp.opts.t2[,1]>=20,]
+aic.tornio2 <- rep(NA, nrow(bp.opts.t2))
+for(i in 1:nrow(bp.opts.t2)){
+# for(i in 1:50){
+	t.bp.t <- bp.opts.t2[i,]
+	t.bp.year.t1 <- tornio[t.bp.t[1],"year"]
+	t.bp.year.t2 <- tornio[t.bp.t[2],"year"]
+	t.tornio <- tornio
+	t.tornio[,"bp1"] <- (1:nrow(t.tornio))>=t.bp.t[1]
+	t.tornio[,"bp2"] <- (1:nrow(t.tornio))>=t.bp.t[2]
+	tobit.tornio.year <- lm(doy ~ year + pmax(I(year-t.bp.year.t1),0) + pmax(I(year-t.bp.year.t2),0), data=t.tornio) # 1867
+	aic.tornio2[i] <- extractAIC(tobit.tornio.year)[2]
+}
+
+tornio[bp.opts.t2[which.min(aic.tornio2),],"year"]
 
 
 
