16d8aaa789f25a9975332f4ef98c5b4976be1228
Ryan Batt <battrd@gmail.com>
2014-12-13 22:31:43 -0500

    update suwa plot

--- R/Figures/plot.bpLikes.R
+++ R/Figures/plot.bpLikes.R
@@ -6,9 +6,10 @@ load("/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Results/tornio
 load("/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Results/suwaBP.RData")
 
 
-# ===============================================================
-# = Plot time series with breakpoint and residual variance/ R^2 =
-# ===============================================================
+
+# ============================================
+# = Plot time series with breakpoint and AIC =
+# ============================================
 # ========
 # = Suwa: 
 # ========
@@ -17,12 +18,13 @@ png("/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Figures/suwa.bp
 par(mar=c(2.5, 2.5, 0.5, 2.5), mgp=c(1.5, 0.5, 0), tcl=-0.35, ps=9, cex=1)
 plot(suwa[bp.opts.s,"year"], suwa[bp.opts.s,"doy"], type="l", xlab="Year", ylab="Ice Breakup Day of Year")
 par(new=TRUE)
-plot(suwa[bp.opts.s, "year"], resid.vars, type="l", col="blue", xaxt="n", yaxt="n", xlab="", ylab="", lwd=2.5)
-lines(suwa[bp.opts.s, "year"], resid.vars, col="white", lwd=1)
+plot(suwa[bp.opts.s, "year"], aic.suwa, type="l", col="blue", xaxt="n", yaxt="n", xlab="", ylab="", lwd=2.5)
+lines(suwa[bp.opts.s, "year"], aic.suwa, col="white", lwd=1)
 axis(side=4)
-mtext(bquote(residual~~variance), side=4, line=1.5)
+mtext(bquote(AIC), side=4, line=1.5)
 abline(v=suwa.bp, lwd=2, lty="dashed")
 dev.off()
+
 
 # ==============================================
 # = Plot time series and breakpoint likelihood =
