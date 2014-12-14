dc0e88e0a4c0ea934f28e85ae97cb7f286691ace
Ryan Batt <battrd@gmail.com>
2014-12-13 22:30:31 -0500

    begin tests for suwa 2BP's, 0 BP, & year+year^2
    need to continue to work on suwa 2BP's. Can't do exhaustive search, ~100,000+ combinations, even if I limit to years 100 years apart. Playing around with integer optimization. Found solution. However, noticing a consistent preference for choosing 2 BP's that are very close to each other.
    
    Creating a soft restriction for BP's to be 25 years apart (a penalty exists [10 AIC points] for years 1 year apart, and decays to 0 penalty once 25 years apart). It found 2 BP years 25 years apart, and AIC was 3510, ~3 AIC points better than 1 BP model.
    
    Need to run hard restrictions for 50 years and 100 years. Then can probably have a better idea if 2 BP's is really better than 1. (Seems to me like some sort of penalty is reasonable here)

--- R/suwaBP.R
+++ R/suwaBP.R
@@ -3,6 +3,8 @@
 # = Load Libraries =
 # ==================
 library(VGAM)
+library(DEoptim)
+library(rgenoud)
 
 
 # =============
@@ -19,31 +21,25 @@ suwa[,"year2"] <- 1:nrow(suwa)
 # =========================
 # = Breakpoint with Tobit =
 # =========================
+AIC(vglm(doy ~ year, tobit(Lower=min.suwa, Upper=max.suwa), data=suwa)) # No breakpoint AIC = 3536.38
+AIC(vglm(doy ~ year + I(year^2), tobit(Lower=min.suwa, Upper=max.suwa), data=suwa)) # No breakpoint +year^2 AIC = 3515.407
+
 # =========================================
 # = Suwa: Calculate Breakpoint with Tobit =
 # =========================================
 bp.opts.s <- 10:(nrow(suwa)-10)
-resid.vars <- rep(NA, length(bp.opts.s))
 s.bp.pb <- txtProgressBar(min=1, max=length(bp.opts.s), style=3)
-nll.s <- rep(NA, length(bp.opts.s))
+aic.suwa <- rep(NA, length(bp.opts.s))
 for(i in 1:length(bp.opts.s)){
 	t.bp <- bp.opts.s[i]
 	t.bp.year <- suwa[t.bp,"year"]
-	t.suwa <- suwa
-	t.suwa[,"bp"] <- (1:nrow(t.suwa))>=t.bp
+	# t.suwa <- suwa
+	# t.suwa[,"bp"] <- (1:nrow(t.suwa))>=t.bp
 	
-	# tobit.suwa.year <- vglm(doy~year2*bp, tobit(Lower=min.suwa, Upper=max.suwa), data=t.suwa) # 1807
-	# tobit.suwa.year <- vglm(doy ~ year:bp, tobit(Lower=min.suwa, Upper=max.suwa), data=t.suwa) # 1842
-	tobit.suwa.year <- vglm(doy ~ year + pmax(I(year-t.bp.year), 0), tobit(Lower=min.suwa, Upper=max.suwa), data=t.suwa) # 1812
-	
-	
-	resid.vars[i] <- exp(coef(tobit.suwa.year)[2])
-	nll.s[i] <- tobit.suwa.year@criterion$loglikelihood
-	
-	min.sofar <- which.min(resid.vars)
-	if(min.sofar!=i){
-		min.tobit.suwa.year <- tobit.suwa.year
-	}
+	tobit.suwa.year <- vglm(doy ~ year + pmax(I(year-t.bp.year), 0), tobit(Lower=min.suwa, Upper=max.suwa), data=suwa) # 1812
+
+	aic.suwa[i] <- AIC(tobit.suwa.year) # Best AIC w/ 1 BP: 3513.241 (just a little over 2 AIC points better than year + year^2)
+
 	setTxtProgressBar(s.bp.pb, i)
 }
 
@@ -51,13 +47,101 @@ for(i in 1:length(bp.opts.s)){
 # =================================
 # = Define Breakpoint and Indices =
 # =================================
-suwa.bp <- suwa[bp.opts.s[which.min(resid.vars)],"year"]
+suwa.bp <- suwa[bp.opts.s[which.min(aic.suwa)],"year"]
 suwa.bp
 suwa.bp.i <- suwa[,"year"] < suwa.bp # indices in units of year2
+
+
+# ===============
+# = Suwa 2 BP's =
+# ===============
+bp.opts.s2 <- t(combn(1:nrow(suwa), 2))
+# s.bp.pb2 <- txtProgressBar(min=1, max=nrow(bp.opts.s2), style=3)
+# aic.suwa2 <- rep(NA, nrow(bp.opts.s2))
+# suwa2.skip <- !((bp.opts.s2[,2]-bp.opts.s2[,1])>=100 & bp.opts.s2[,1]>=10 & bp.opts.s2[,2]<=(nrow(suwa)-10))
+# for(i in 1:nrow(bp.opts.s2)){
+# 	if(suwa2.skip[i]){
+# 		setTxtProgressBar(s.bp.pb2, i)
+# 		next
+# 	}
+# 	t.bp <- bp.opts.s2[i,]
+# 	t.bp.year1 <- suwa[t.bp[1],"year"]
+# 	t.bp.year2 <- suwa[t.bp[2],"year"]
+# 	# t.suwa <- suwa
+# # 	t.suwa[,"bp1"] <- (1:nrow(t.suwa))>=t.bp[1]
+# # 	t.suwa[,"bp2"] <- (1:nrow(t.suwa))>=t.bp[2]
+# #
+# 	tobit.suwa.year <- vglm(doy ~ year + pmax(I(year-t.bp.year1), 0) + pmax(I(year-t.bp.year2), 0), tobit(Lower=min.suwa, Upper=max.suwa), data=suwa) # 1812
+#
+# 	aic.suwa2[i] <- AIC(tobit.suwa.year)
+#
+# 	setTxtProgressBar(s.bp.pb2, i)
+# }
+
+suwa.2bp.nll.25 <- function(bps){
+	if(bps[2]<=bps[1]){
+		return(9E9)
+	}
+
+	penalty <- 10 - (bps[2] - bps[1])*0.4 # no penalty at 25 years
+	penalty <- max(penalty, 0)
+	
+	t.bp.year1 <- suwa[bps[1],"year"]
+	t.bp.year2 <- suwa[bps[2],"year"]
+	
+	tobit.suwa.year <- vglm(doy ~ year + pmax(I(year-t.bp.year1), 0) + pmax(I(year-t.bp.year2), 0), tobit(Lower=min.suwa, Upper=max.suwa), data=suwa) # 1812
+
+	AIC(tobit.suwa.year) + penalty
+}
+
+
+suwa.2bp.nll.50 <- function(bps){
+	if(bps[2]<=bps[1]){
+		return(9E9)
+	}
+	
+	if((bps[2]-bps[1])<50){
+		return(9E9)
+	}
+	
+	t.bp.year1 <- suwa[bps[1],"year"]
+	t.bp.year2 <- suwa[bps[2],"year"]
+	
+	tobit.suwa.year <- vglm(doy ~ year + pmax(I(year-t.bp.year1), 0) + pmax(I(year-t.bp.year2), 0), tobit(Lower=min.suwa, Upper=max.suwa), data=suwa) # 1812
+
+	AIC(tobit.suwa.year) #+ penalty
+}
+
+
+suwa.2bp.nll.100 <- function(bps){
+	if(bps[2]<=bps[1]){
+		return(9E9)
+	}
+
+	if((bps[2]-bps[1])<100){
+		return(9E9)
+	}
+	
+	t.bp.year1 <- suwa[bps[1],"year"]
+	t.bp.year2 <- suwa[bps[2],"year"]
+	
+	tobit.suwa.year <- vglm(doy ~ year + pmax(I(year-t.bp.year1), 0) + pmax(I(year-t.bp.year2), 0), tobit(Lower=min.suwa, Upper=max.suwa), data=suwa) # 1812
+
+	AIC(tobit.suwa.year) #+ penalty
+}
+
+# suwa.2bp.optim.de <- DEoptim(suwa.2bp.nll, c(10,10), c(nrow(suwa)-10, nrow(suwa)-10), control=DEoptim.control(itermax=5, NP=500))
+dom <- matrix(c(10,nrow(suwa)-11, 11, nrow(suwa)-10), ncol=2, byrow=TRUE)
+suwa.2bp.optim.gen.25 <- genoud(suwa.2bp.nll.25, 2, pop.size=500, max.generations=50, data.type.int=TRUE, Domains=dom, boundary.enforcement=2) # with soft penalty for BP < 25 yrs apart, BP's are 1828 (386) and 1853 (411); AIC = 3510.898
+
+suwa.2bp.optim.gen.50 <- genoud(suwa.2bp.nll.50, 2, pop.size=500, max.generations=50, data.type.int=TRUE, Domains=dom, boundary.enforcement=2)
+
+suwa.2bp.optim.gen.100 <- genoud(suwa.2bp.nll.100, 2, pop.size=500, max.generations=50, data.type.int=TRUE, Domains=dom, boundary.enforcement=2)
+
 
 
 # ================
 # = Save Results =
 # ================
-save(suwa.bp, suwa.bp.i, resid.vars, suwa, max.suwa, min.suwa, bp.opts.s, file="/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Results/suwaBP.RData")
+save(suwa.bp, suwa.bp.i, aic.suwa, suwa, max.suwa, min.suwa, bp.opts.s, suwa.2bp.optim.gen.25, file="/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Results/suwaBP.RData")
 
