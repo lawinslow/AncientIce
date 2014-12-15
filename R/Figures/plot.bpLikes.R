76eb8fdc26bbf2da6e93c4c8acea1e5650f6a1fd
rbatt <battrd@gmail.com>
2014-12-15 15:17:26 -0500

    alternate way to fill 2BP AIC surface
    correctly implementing line positioning

--- R/Figures/plot.bpLikes.R
+++ R/Figures/plot.bpLikes.R
@@ -1,4 +1,11 @@
 
+# ==================
+# = Load Libraries =
+# ==================
+library(fields)
+library(reshape2)
+
+
 # ================
 # = Load Results =
 # ================
@@ -48,9 +55,19 @@ dev.off()
 myCol <- function(n){
 	colorRampPalette(c("#000099", "#00FEFF", "#45FE4F", "#FCFF00", "#FF9400", "#FF3100"))(n)
 }
-bp2.mat <- matrix(NA, nrow=nrow(tornio), ncol=nrow(tornio), dimnames=list(unique(tornio[,"year"]), unique(tornio[,"year"])))
-bp2.mat[t(upper.tri(bp2.mat))] <- aic.tornio2
-bp2.mat <- t(bp2.mat)
+# bp2.mat <- matrix(NA, nrow=nrow(tornio), ncol=nrow(tornio), dimnames=list(unique(tornio[,"year"]), unique(tornio[,"year"])))
+# bp2.mat[t(upper.tri(bp2.mat))] <- aic.tornio2
+# bp2.mat <- t(bp2.mat)
+
+df <- as.data.frame(cbind(bp.opts.t2, aic.tornio2))
+names(df) <- c("year1","year2","aic")
+
+df0 <- expand.grid(1:nrow(tornio), 1:nrow(tornio))
+names(df0) <- c("year1", "year2")
+
+df.fill <- merge(df0, df, all=TRUE)
+names(df.fill) <- c("year1","year2","value")
+bp2.mat <- as.matrix(dcast(df.fill, year1~year2)[,-1])
 
 smplt <- c(0.8,0.85, 0.15,0.75)
 bgplt <- c(0.1,0.95,0.1,0.95)
@@ -59,15 +76,16 @@ axargs <- list(mgp=c(1.1,0.3,0))
 png("/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce/Figures/tornio.2BP.surface.png", width=5, height=5, res=150, units="in")
 par(mar=c(2,2,0.1,2), ps=10, cex=1, family="Times", mgp=c(1.25,0.25,0), tcl=-0.15)
 image.plot(
-	x=as.integer(rownames(bp2.mat)), 
-	y=as.integer(colnames(bp2.mat)), 
+	x=as.integer(unique(tornio[,"year"])), #as.integer(rownames(bp2.mat)), 
+	y=as.integer(unique(tornio[,"year"])), #as.integer(colnames(bp2.mat)), 
 	z=bp2.mat-min(aic.tornio), 
 	xlab=bquote(1^st~~Breakpoint), 
 	ylab=bquote(2^nd~~Breakpoint), 
-	legend.lab=bquote(Delta~AIC~~(Relative~~to~~Best~~1~~BP~~Model)),
+	# legend.lab=bquote(Delta~AIC~~(Relative~~to~~Best~~1~~BP~~Model)),
 	smallplot=smplt, 
 	bigplot=bgplt,
-	legend.line=1,
+	legend.args=list(text=bquote(Delta~AIC~~(Relative~~to~~Best~~1~~BP~~Model)), line=1, side=4),
+	# legend.line=1,
 	axis.args=axargs
 )
 abline(a=0, b=1)
@@ -76,4 +94,5 @@ abline(a=100, b=1)
 text(1850, 1850, bquote(phantom()%up%phantom()~~BP>=1~~yr~~apart~~bold(phantom()%up%phantom())), srt=45, pos=3, offset=0)
 text(1850, 1900, bquote(phantom()%up%phantom()~~BP>=50~~yrs~~apart~~bold(phantom()%up%phantom())), srt=45, pos=3, offset=0)
 text(1850, 1950, bquote(phantom()%up%phantom()~~BP>=100~~yrs~~apart~~bold(phantom()%up%phantom())), srt=45, pos=3, offset=0)
+
 dev.off() 
