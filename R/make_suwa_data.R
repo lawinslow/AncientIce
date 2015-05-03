40a5fd414cd46e1fc0d52a43dc2fec4aafd81150
Ryan Batt <battrd@gmail.com>
2015-05-03 15:26:24 -0400

    customary to start paths with project folder?

--- R/make_suwa_data.R
+++ R/make_suwa_data.R
@@ -8,7 +8,7 @@
 #ice.new <- read.table("./AncientIce/lib/ice_data_prep/data/suwa_prepared_analysis_data.csv", sep=",", header=TRUE)
 
 #pull from web (could change this to pull from submodule, but I always have trouble with them)
-ice.new <- read.csv('lib/ice_data_prep/data/suwa_prepared_analysis_data.csv', header=TRUE, as.is=TRUE)
+ice.new <- read.csv('./AncientIce/lib/ice_data_prep/data/suwa_prepared_analysis_data.csv', header=TRUE, as.is=TRUE)
 
 
 #subtract our iceon date from Jan 0 of that year
@@ -22,7 +22,7 @@ ice.new$no.ice <- NA
 ice.new$no.ice[ice.new$froze == 'Y'] <- 0
 ice.new$no.ice[ice.new$froze == 'N'] <- 1
 
-suwa.old <- read.table("Data/suwa.tsv", sep="\t", header=TRUE)
+suwa.old <- read.table("./AncientIce/Data/suwa.tsv", sep="\t", header=TRUE)
 
 
 suwa.new <- ice.new[, c('rule_year', 'no.ice', 'doy')]
