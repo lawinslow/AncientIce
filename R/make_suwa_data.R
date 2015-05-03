3a0bc63d01ee1eeabbfb69f84dbee4f6710040be
Ryan Batt <battrd@gmail.com>
2015-05-03 12:13:46 -0400

    get new suwa data into old format
    @lawinslow help here?

---
+++ R/make_suwa_data.R
@@ -0,0 +1,9 @@
+
+# ==========
+# = Set WD =
+# ==========
+setwd("/Users/Battrd/Documents/School&Work/WiscResearch") # for ryan
+
+suwa <- read.table("./AncientIce/lib/ice_data_prep/data/suwa_prepared_analysis_data.csv", sep=",", header=TRUE)
+
+
