213cbaa7f2783afb470dcc36427128d3cc2985ae
Ryan Batt <battrd@gmail.com>
2015-05-03 12:18:52 -0400

    starting suwa update
    @lawinslow if you get to doing this, text me and I'll push my progress

--- R/make_suwa_data.R
+++ R/make_suwa_data.R
@@ -4,6 +4,9 @@
 # ==========
 setwd("/Users/Battrd/Documents/School&Work/WiscResearch") # for ryan
 
-suwa <- read.table("./AncientIce/lib/ice_data_prep/data/suwa_prepared_analysis_data.csv", sep=",", header=TRUE)
+ice.new <- read.table("./AncientIce/lib/ice_data_prep/data/suwa_prepared_analysis_data.csv", sep=",", header=TRUE)
+suwa.index <- ice.new[,"lakename"]=="LAKE SUWA" 
+suwa.new <- ice.new[]
 
 
+suwa.old <- read.table("./AncientIce/Data/suwa.tsv", sep="\t", header=TRUE)
