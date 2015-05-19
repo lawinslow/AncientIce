891fdb41cd4e358dcedcc52febbdbb2d084eb283
Ryan Batt <battrd@gmail.com>
2015-05-19 12:21:34 -0400

    compare NAO via scatter plot of 2 data sources

--- R/nao_djfm.R
+++ R/nao_djfm.R
@@ -22,3 +22,29 @@ points(other_nao$year, other_nao$nao.djfm, col=rgb(1,0,0,0.7))
 tmp = filter(other_nao, year > 1850)
 points(tmp$year, tmp$nao.djfm, pch=21, col=rgb(1,0,0,0.7), bg=rgb(0,0,0,0.4))
 dev.off()
+
+
+
+# ======================================================
+# = Scatter Plot of NAO Indices from Different Sources =
+# ======================================================
+
+# Combine data source
+other_nao2 <- other_nao[,c("nao.djfm","year")]
+names(other_nao2) <- c("torne_nao.djfm", "Year")
+nao <- merge(nao_djfm, other_nao2, all=TRUE)
+
+
+# Make plot
+png('Figures/NAO_djfm_compare_scatter.png', width=2500, height=1800, res=300)
+
+plot(nao[,"nao_djfm"], nao[,"torne_nao.djfm"], col=c("black","red")[1+(nao[,"Year"]>=1850)], xlab="Luterbacher DJFM NAO", ylab="Torne data set DJFM NAO")
+
+legend("topleft", legend=c("Up to 1850", "After 1850"), pch=21, col=c("black","red"))
+
+abline(lm(torne_nao.djfm~nao_djfm, data=nao[nao[,"Year"]>=1850,]), col="black")
+abline(lm(torne_nao.djfm~nao_djfm, data=nao[nao[,"Year"]<1850,]), col="red")
+
+
+
+dev.off()
