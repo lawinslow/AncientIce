1a5169b8241e882f4afad03d868f6e8db90c58a8
Ryan Batt <battrd@gmail.com>
2015-10-04 22:30:34 -0400

    do tobit on suwa manually-defined Bef Aft

---
+++ R/Suwa_BeforeAfter.R
@@ -0,0 +1,103 @@
+
+
+# ==================
+# = Load Libraries =
+# ==================
+library(VGAM)
+library(rgenoud)
+library(MCMCpack)
+
+# ==========
+# = Set WD =
+# ==========
+setwd("/Users/Battrd/Documents/School&Work/WiscResearch/AncientIce") # for ryan
+
+
+# ==================
+# = Load Functions =
+# ==================
+func.location <- "R/Functions"
+invisible(sapply(paste(func.location, list.files(func.location), sep="/"), source, .GlobalEnv))
+
+
+# =============
+# = Load Data =
+# =============
+suwa <- read.table("Data/suwa.tsv", sep="\t", header=TRUE)
+max.suwa <- max(suwa[,"doy"], na.rm=TRUE)
+min.suwa <- -Inf #min(suwa[,"doy"], na.rm=TRUE)
+
+suwa.no.ice <- suwa[,"no.ice"]==1L & !is.na(suwa[,"no.ice"]) # indexes years known to not freeze (which also means that we can't know the "doy" [feeze date] b/c it doesn't exist)
+suwa[suwa.no.ice ,"doy"] <- max.suwa # for no freeze years; note that the answer is the same if we replace with (max.suwa) vs. with (max.suwa+100) [or any other positive increase]
+
+# suwa.nodoy.freeze <- suwa[,"no.ice"]==0L & !is.na(suwa[,"no.ice"]) & is.na(suwa[,"doy"]) # indexes years
+# suwa[suwa.nodoy.freeze,"doy"] <- min.suwa
+
+
+suwa[,"year2"] <- 1:nrow(suwa)
+
+
+# =====================
+# = Define Early Late =
+# =====================
+suwa.early.year <- c(min(suwa[,"year"]), 1655)
+suwa.late.year <- c(1923, max(suwa[,"year"]))
+
+suwa.early.index <- suwa[,"year"]<=suwa.early.year[2]
+suwa.late.index <- suwa[,"year"]>=suwa.late.year[1]
+
+suwa[suwa.early.index,"period"] <- "early"
+suwa[suwa.late.index,"period"] <- "late"
+suwa[!suwa.early.index & !suwa.late.index,"period"] <- "extremeOnly"
+
+
+# =======================
+# = Regression on Early =
+# =======================
+suwa.tobit.early <- vglm(doy~year, tobit(Lower=min.suwa, Upper=max.suwa), na.action=na.exclude, data=suwa[suwa.early.index,])
+
+
+# ======================
+# = Regression on Late =
+# ======================
+suwa.tobit.late <- vglm(doy~year, tobit(Lower=min.suwa, Upper=max.suwa), na.action=na.exclude, data=suwa[suwa.late.index,])
+
+
+# ==================
+# = Calculate CI's =
+# ==================
+
+
+get.suwa.ci <- function(model, data){
+	# year0 <- model@x[,"year"]
+	# year <- seq(min(year0), max(year0))  #suwa[suwa[,"period"]==period,"year"]
+	year <- data[,"year"]
+	
+	# ci0 <- data.frame("year"=year0, "doy"=model@y)
+	# ci <- merge(data.frame(year=year), ci0, all=T) #data.frame("year"=year, "doy"=model@y)
+	ci <- data.frame(year=year, doy=data[,"doy"])
+	
+	newdata <- data.frame("year"=year)
+	
+	se.fit <- data.frame(predict(model, newdata=newdata, se.fit=TRUE, na.action=na.exclude)$se.fit)
+	
+	se.fit[,"year"] <- year #[as.integer(row.names(se.fit))]
+	se.fit[,"fitted"] <- predict(model, newdata=newdata, se.fit=TRUE)$fitted.values[,1]
+	se.fit[,"se"] <- se.fit[,2]
+	se.fit[,"upr"] <- se.fit[,"fitted"] + se.fit[,1]*1.96
+	se.fit[,"lwr"] <- se.fit[,"fitted"] - se.fit[,1]*1.96
+	
+	ci <- merge(ci, se.fit, by="year", all=TRUE)
+	
+	return(ci)
+}
+
+suwa.ci.early <- get.suwa.ci(suwa.tobit.early, suwa[suwa.early.index,])
+suwa.ci.late <- get.suwa.ci(suwa.tobit.late, suwa[suwa.late.index,])
+
+
+# =============================
+# = Save Before After Results =
+# =============================
+save(suwa, suwa.ci.early, suwa.ci.late, suwa.early.index, suwa.late.index, file="./Results/Suwa_BeforeAfter.RData")
+
